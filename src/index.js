4d47892d45de2ea695c1fe5b90b4e8ad2810a677
Abdalla Alhag <alhagabdalla@gmail.com>
2024-05-24 12:21:46 -0700

    Changed pics, Fixed github Icon, working on scaling

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import './style.css';
 import { createHome } from "./home.js";
 import { createMenu } from "./menu.js";
 import { createContact } from "./contact.js";
+import githubIcon from './img/github-mark.png';
 
 createHome();
+document.getElementById('github-icon').innerHTML = `<img src="${githubIcon}" alt="github icon" />`;
 
 const homeBtn = document.querySelector('#home');
 const menuBtn = document.querySelector('#menu');
